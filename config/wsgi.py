55d7dda4037956ea9f96e25148b75c3142969ea2
yuu-eguci <yuu.eguci@gmail.com>
2021-04-03 13:48:53 +0900

    🔧 AppService 環境用設定: manage.py wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# App Service 環境では config.production を使います。
+# NOTE: WEBSITE_HOSTNAME は App Service 環境のデフォルト環境変数です。
+# NOTE: manage.py にも同様の設定があります。
+settings_module = 'config.production' if 'WEBSITE_HOSTNAME' in os.environ else 'config.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,13 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    # App Service 環境では config.production を使います。
+    # NOTE: WEBSITE_HOSTNAME は App Service 環境のデフォルト環境変数です。
+    # NOTE: wsgi.py にも同様の設定があります。
+    settings_module = 'config.production' if 'WEBSITE_HOSTNAME' in os.environ else 'config.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
