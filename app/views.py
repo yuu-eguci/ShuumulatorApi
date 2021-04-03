e84e2a07ce6b29e728dcdd659e09e227ca9a707b
yuu-eguci <yuu.eguci@gmail.com>
2021-04-03 13:53:22 +0900

    🔧 AppService 環境用設定: my_customized_server_error

--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import (
+    HttpResponseServerError,
+)
 
 # Create your views here.
+
+
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+
+    # NOTE: print が App Service で機能するかどうか確かめるために print しています。
+    import traceback
+    print(traceback.format_exc())
+    # return HttpResponseServerError('<h1>Server Error (500)</h1>')
+
+    # DEBUG = True と同様の画面を出します。
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+handler500 = views.my_customized_server_error
